838fd8edbd5d8340dbee3ab967667b00a174efc7
Simon Guichandut <simonguichandut@gmail.com>
2024-12-10 11:40:49 -0500

    simpler day 10, only keep track of score

--- 10.py
+++ 10.py
@@ -15,24 +15,31 @@ grid = [[int(c) for c in line] for line in data]
 heads = []
 for r in range(R):
     for c in range(C):
-        if grid[r][c] == 0:
-            heads.append((r,c))
+        if grid[r][c] == 0: heads.append((r,c))
 
-def dfs(node, visited=set(), paths=[], pt2=False):
+def dfs(node, visited=None, score=None, pt2=False):
+
+    if visited==None: # and paths
+        # For some reason it doesn't work to set these as the default arguments to the function
+        # It won't re-initialize as the defaults on successive calls
+        visited = set()
+        score = 0
+
     visited.add(node)
     r,c = node
 
     if grid[r][c] == 9:
-        paths.append(len(visited))
+        score += 1
 
     else:
         for (dr,dc) in ((1,0),(-1,0),(0,1),(0,-1)):
             if r+dr in range(R) and c+dc in range(C) and grid[r+dr][c+dc]-grid[r][c]==1 and (r+dr,c+dc) not in visited:
-                dfs(((r+dr),(c+dc)), visited, paths, pt2)
+                score = dfs(((r+dr),(c+dc)), visited, score, pt2)
     
     if pt2:
         visited.remove(node)
-    return paths
 
-print("Part 1: ", sum(len(dfs(head, visited=set(), paths=[])) for head in heads))
-print("Part 2: ", sum(len(dfs(head, visited=set(), paths=[], pt2=True)) for head in heads))
+    return score
+
+print("Part 1: ", sum(dfs(head) for head in heads))
+print("Part 2: ", sum(dfs(head, pt2=True) for head in heads))
