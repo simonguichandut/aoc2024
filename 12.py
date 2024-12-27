1c61ae6f37553818ad3c66ecc167262c1145a0cf
Simon Guichandut <simonguichandut@gmail.com>
2024-12-27 12:37:25 -0500

    day12 done

--- 12.py
+++ 12.py
@@ -1,56 +1,58 @@
 from utils import *
-from collections import defaultdict, deque
+from collections import defaultdict, deque              
 from functools import cache
 
 garden = read_lines()
-R, C = len(garden), len(garden[0])
+R, C = len(garden), len(garden[0])     
+                                                                         
+# Traverse the whole grid                               
+# If never seen this node before, perform BFS to find the whole region                                      
+# Area is just the number of elements in the region. Add one to perimeter every time a neighbour is not part of the region.
 
-# Traverse the whole grid
-# If never seen this node before, perform BFS to find the whole region
-# Area is just the number of elements in the region, need to calculate perimeter
-ans = 0
-seen_all = set()
-for r in range(R):
+# hyperneutrino's corner method for pt2
+def sides(region):
+    corner_coords = set()
+    for (r,c) in region:
+        for rr,cc in [(r-0.5, c+0.5), (r+0.5, c+0.5), (r+0.5, c-0.5), (r-0.5, c-0.5)]:
+            corner_coords.add((rr,cc))
+    num_sides = 0
+    for rr,cc in corner_coords:
+        config = [(r,c) in region for (r,c) in [(rr-0.5, cc+0.5), (rr+0.5, cc+0.5), (rr+0.5, cc-0.5), (rr-0.5, cc-0.5)]]
+        match sum(config):
+            case 1:
+                num_sides += 1
+            case 2:
+                if config == [True, False, True, False] or config == [False, True, False, True]:
+                    num_sides += 2
+            case 3:
+                num_sides += 1
+    return num_sides
+
+ans1 = ans2 = 0
+seen = set()
+for r in range(R):                                          
     for c in range(C):
-        if (r,c) not in seen_all:
-
-            Q = deque()
+        if (r,c) not in seen:
+              
+            Q = deque()                                             
             Q.append((r,c))
-            letter = garden[r][c]
-            
+            letter = garden[r][c]                       
             perim = 0
-            seen = set()
+            region = set()
             while Q:
-                r,c = Q.popleft()
-                if (r,c) not in seen:
-                    seen.add((r,c))
+                rr,cc = Q.popleft()
+                if (rr,cc) not in region:
+                    region.add((rr,cc))
                     for (dr,dc) in adj4:
-                        if (r+dr,c+dc) in seen: continue
-                        if r+dr in range(R) and c+dc in range(C) and garden[r+dr][c+dc] == letter:
-                            Q.append((r+dr,c+dc))
-                        else: 
+                        if (rr+dr,cc+dc) in region: continue
+                        if rr+dr in range(R) and cc+dc in range(C) and garden[rr+dr][cc+dc] == letter:
+                            Q.append((rr+dr,cc+dc))
+                        else:
                             perim += 1
 
-            # regions.append(list(seen))
-            seen_all |= seen
+            ans1 += len(region) * perim
+            ans2 += len(region) * sides(region)
+            seen |= region
 
-            # update answer
-            print(letter, len(seen), perim)
-            ans += len(seen) * perim
-
-print("Part 1: ", ans)
-#1432742
-
-# Now just need a function to determine the perimeter
-# Actually you can always unfold the perimeter into a rectangle
-# except no this doesn't always work if there's holes or concavity
-def perim(region):
-    rows, cols = [x for x in zip(*region)] # basically a transpose
-    height, width = max(rows)-min(rows)+1, max(cols)-min(cols)+1
-    return 2*(height+width)
-
-
-# for region in regions:
-#     r0,c0 = region[0]
-#     print(garden[r0][c0], len(region), perim(region))
-#     # print(len(region))
+print("Part 1: ", ans1)
+print("Part 2: ", ans2)
