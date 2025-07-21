3d6c0727e9c436ad16505d1b522e585c82d6812c
Simon Guichandut <simonguichandut@gmail.com>
2025-07-21 13:25:45 -0400

    day 18 more efficient

--- 18.py
+++ 18.py
@@ -1,16 +1,16 @@
 from utils import *
-from collections import deque
+from collections import deque,defaultdict
 
 corrupted = [tuple(map(int, line.split(','))) for line in read_lines()]
 
-def print_grid(L, num_corrupted, visited):
+def print_grid(L, num_corrupted, marked):
     grid = []
     for r in range(L):
         line = ''
         for c in range(L):
             if (c,r) in corrupted[:num_corrupted]:
                 line += '#'
-            elif (r,c) in visited:
+            elif (r,c) in marked:
                 line += 'O'
             else:
                 line += '.'
@@ -19,12 +19,13 @@ def print_grid(L, num_corrupted, visited):
 
 def bfs(L, num_corrupted, print_result=False):
 
-    start = (0,0,0)
+    start = (0,0)
     Q = deque([start])
     visited = set(start)
+    parents = defaultdict(None)
 
     while Q:
-        r,c,n = Q.popleft()
+        r,c = Q.popleft()
         if (r,c) == (L-1,L-1):
             break
         for (dr,dc) in adj4:
@@ -33,26 +34,44 @@ def bfs(L, num_corrupted, print_result=False):
             if (ndc,ndr) in corrupted[:num_corrupted]: continue
             if (ndr,ndc) in visited: continue
             visited.add((ndr,ndc))
-            Q.append((ndr,ndc,n+1))
-
-    if print_result:
-        print_grid(L, num_corrupted, visited)
+            parents[(ndr,ndc)] = (r,c)
+            Q.append((ndr,ndc))
                 
     if (L-1,L-1) in visited:
-        return n
-    
-print("Part 1: ", bfs(71, 1024))
+        curr = (L-1,L-1)
+        shortest = [curr]
+        while parents[curr]:
+            curr = parents[curr]
+            if curr == start:
+                break
+            shortest.append(curr)
 
-# part 2 brute force...
+        if print_result:
+            print_grid(L, num_corrupted, shortest)
+
+        return shortest
+
+shortest = bfs(71, 1024)
+print("Part 1: ", len(shortest))
+
 for i in range(1023, len(corrupted)):
     print(i, end='\r')
-    if not bfs(71,i):
+    if corrupted[i-1][::-1] not in shortest:
+        continue
+    shortest = bfs(71,i)
+    if shortest is None:
         print("Part 2: ", ','.join(str(a) for a in corrupted[i-1]))
         break
 
 # Test input
-#print("Part 1: ", bfs(7, 12))
-#for i in range(13, len(corrupted)):
-#    if not bfs(7,i):
-#        print("Part 2: ", ','.join(str(a) for a in corrupted[i-1]))
-#        break
+# shortest = bfs(7, 12)
+# print("Part 1: ", len(shortest))
+
+# for i in range(13, len(corrupted)):
+#     print(i, end='\r')
+#     if corrupted[i-1][::-1] not in shortest:
+#         continue
+#     shortest = bfs(7,i)
+#     if shortest is None:
+#         print("Part 2: ", ','.join(str(a) for a in corrupted[i-1]))
+#         break
